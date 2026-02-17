195de4b37203004e76e1b0e514f6174fdc572695
rcc00n <vadrud2016@gmail.com>
2026-02-17 14:10:00 -0700

    test(orders): add delivery quote tests for supported and unsupported areas

---
+++ backend/orders/tests/test_utils.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from orders.utils import DeliveryZoneError, get_delivery_quote
+
+
+class DeliveryQuoteTests(TestCase):
+    def test_get_delivery_quote_matches_by_city_keyword(self):
+        quote = get_delivery_quote(
+            address_line1="123 Any St",
+            city="St. Albert",
+            postal_code="T5A1A1",
+            now=timezone.make_aware(datetime(2026, 2, 1, 10, 0, 0)),
+        )
+
+        self.assertEqual(quote.service_area, "St. Albert")
+        self.assertEqual(quote.fee_cents, 2000)
+        self.assertIn("Arrives today", quote.eta_text)
+
+    def test_get_delivery_quote_raises_outside_supported_areas(self):
+        with self.assertRaises(DeliveryZoneError):
+            get_delivery_quote(
+                address_line1="500 Unknown Road",
+                city="Calgary",
+                postal_code="T2P1J9",
+            )
